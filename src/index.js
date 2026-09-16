ca276f7bc331bf628b37a001e4d47451f6f23beb
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-09-16 15:44:30 +0200

    added a function to get forecast from VisualCrossing API. The API key in not protected because that's beyond the scope of this project. It's a free API anyway :/

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import "./styles.css";
 
 //https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=DAWA9WNY3RQR747DRG7MDFEFK
 
-let location = "";
 
 function getLocation () {
-    location = document.getElementById("location").value;
+    const location = document.getElementById("location").value;
     console.log(location);
+    getForecast(location);
 }
 
 document.getElementById("search").onclick = getLocation;
@@ -19,3 +19,13 @@ document.getElementById("location").addEventListener("keyup", function(event) {
         document.getElementById("search").click();
     }
 });
+
+async function getForecast (address) {
+    try {
+      const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + address + "?key=DAWA9WNY3RQR747DRG7MDFEFK");
+      const forecast = await response.json();
+      console.log(forecast);
+    } catch (error) {
+      console.error(error);
+    }
+}
