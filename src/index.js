5d135bc959b7374fbd61528a8c2c433be3154a1e
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-09-16 15:30:57 +0200

    added input for location to search

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("test");
+import "./styles.css";
+
+//DAWA9WNY3RQR747DRG7MDFEFK
+
+//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=DAWA9WNY3RQR747DRG7MDFEFK
+
+let location = "";
+
+function getLocation () {
+    location = document.getElementById("location").value;
+    console.log(location);
+}
+
+document.getElementById("search").onclick = getLocation;
+
+document.getElementById("location").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("search").click();
+    }
+});
